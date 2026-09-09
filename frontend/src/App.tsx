d883eb579ad456795dd4923a0b14a73cdec403c2
mohankumaronly <mohankumaronly81@gmail.com>
2026-09-09 14:13:16 +0530

    Refactoring Frontend: react with typescript and tailwindcss project init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl font-bold flex items-center justify-center h-screen'>Rockranger</div>
+  )
+}
+
+export default App
